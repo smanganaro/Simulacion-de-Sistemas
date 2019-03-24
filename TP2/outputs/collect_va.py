a06c2275a1a25c561ccade8290323b5e5451519b
Agustin Vazquez <agvazquez@itba.edu.ar>
2019-03-24 19:36:35 -0300

    nuevos output

--- TP2/outputs/collect_va.py
+++ TP2/outputs/collect_va.py
@@ -2,16 +2,16 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 import subprocess
 
-files = ['6-dynamic2-L-25-N-500.ari','2-dynamic2-L-25-N-100.ari']
-noise = [0.1]
+files = ['1-dynamic-L-25-N-50.ari','2-dynamic-L-25-N-100.ari','3-dynamic-L-25-N-200.ari','4-dynamic-L-25-N-300.ari'
+,'5-dynamic-L-25-N-400.ari','6-dynamic-L-25-N-500.ari','7-dynamic-L-25-N-700.ari','8-dynamic-L-25-N-1000.ari','9-dynamic-L-25-N-3000.ari']
+noise = [0.4]
 s_time = 2000
 
 with ThreadPoolExecutor(max_workers=5) as executor:
     for x in noise:
         values = [x]
         for y in files:
-            command = 'java -jar ../target/off-lattice-1.0-SNAPSHOT-jar-with-dependencies.jar \
-            -df ../dynamic/{file} -rc 1 -t {time} -n {noise} -op false'.format(
+            command = 'java -jar ../target/off-lattice-1.0-SNAPSHOT-jar-with-dependencies.jar -df ../dynamic/{file} -rc 1 -t {time} -n {noise} -op false'.format(
                     file=y,
                     noise=x,
                     time=s_time,
